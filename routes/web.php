b1fb99c77d62accb7f1fb8b49ccc0ee4c8de7024
prabath1998 <prabhathudayanga1998@gmail.com>
2023-11-01 22:12:51 +0530

    add controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CityController;
+use App\Http\Controllers\CountryController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StateController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -42,8 +48,14 @@ Route::middleware('splade')->group(function () {
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
 
-    Route::middleware('auth')->group(function () {
-        Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
+    Route::middleware('auth')->prefix('/admin')->name('admin.')->group(function () {
+        Route::get('/',[AdminController::class,'index'])->name('admin.index');
+        Route::resource('/users',UserController::class);
+        Route::resource('/employees', EmployeeController::class);
+        Route::resource('/countries', CountryController::class);
+        Route::resource('/states', StateController::class);
+        Route::resource('/cities', CityController::class);
+        Route::resource('/departments', DepartmentController::class);
     });
 
     require __DIR__.'/auth.php';
