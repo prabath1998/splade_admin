7f046b0e63e519371275b0c2df82c48196b2d944
prabath1998 <prabhathudayanga1998@gmail.com>
2023-11-05 14:22:20 +0530

    add cities

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -2,7 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateCityRequest;
+use App\Models\City;
+use App\Models\State;
+use App\Tables\Cities;
 use Illuminate\Http\Request;
+use ProtoneMedia\Splade\Facades\Toast;
+use ProtoneMedia\Splade\FormBuilder\Input;
+use ProtoneMedia\Splade\FormBuilder\Select;
+use ProtoneMedia\Splade\FormBuilder\Submit;
+use ProtoneMedia\Splade\SpladeForm;
 
 class CityController extends Controller
 {
@@ -11,7 +20,9 @@ class CityController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.cities.index', [
+            'cities' => Cities::class
+        ]);
     }
 
     /**
@@ -19,15 +30,36 @@ class CityController extends Controller
      */
     public function create()
     {
-        //
+        $form = SpladeForm::make()
+            ->action(route('admin.cities.store'))
+            ->class('space-y-4 p-4 bg-white rounded')
+            ->fields([
+                Input::make('name')->label('Name'),
+                Select::make('state_id')
+                    ->label('Choose a state')
+                    ->options(State::pluck('name', 'id')->toArray()),
+                Submit::make()->label('Save')
+            ]);
+
+        return view('admin.cities.create', [
+            'form' => $form
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateCityRequest $request)
     {
-        //
+        City::create($request->validated());
+
+        Toast::title('Created!')
+            ->message('New city created successfully')
+            ->success()
+            ->rightTop()
+            ->backdrop()
+            ->autoDismiss(1);
+        return to_route('admin.cities.index');
     }
 
     /**
@@ -41,24 +73,55 @@ class CityController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(City $city)
     {
-        //
+        $form = SpladeForm::make()
+        ->action(route('admin.cities.update', $city))
+        ->method('PUT')
+        ->class('space-y-4 p-4 bg-white rounded')
+        ->fill($city)
+        ->fields([
+            Input::make('name')->label('Name'),
+            Select::make('state_id')
+                ->label('Choose a state')
+                ->options(State::pluck('name', 'id')->toArray()),
+            Submit::make()->label('Save')
+        ]);
+
+    return view('admin.cities.edit', [
+        'form' => $form
+    ]);
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(CreateCityRequest $request, City $city)
     {
-        //
+        $city->update($request->validated());
+
+        Toast::title('Updated!')
+            ->message('City updated successfully')
+            ->success()
+            ->rightTop()
+            ->backdrop()
+            ->autoDismiss(1);
+        return to_route('admin.cities.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(City $city)
     {
-        //
+        $city->delete();
+        Toast::title('Deleted!')
+        ->message('City deleted successfully')
+        ->success()
+        ->rightTop()
+        ->backdrop()
+        ->autoDismiss(1);
+    return back();
     }
 }
---
+++ resources/views/admin/cities/index.blade.php
@@ -0,0 +1,19 @@
+<x-admin-layout>
+    <div class="flex justify-between">
+        <h1 class="text-2xl font-semibold p-4">Cities Index</h1>
+        <div class="p-4">
+            <Link href="{{ route('admin.cities.create') }}"
+                class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded text-white">New City</Link>
+        </div>
+    </div>
+    <x-splade-table :for="$cities">
+        @cell('action', $city)
+            <div class="space-x-2">
+                <Link href="{{ route('admin.cities.edit', $city) }}"
+                    class="px-3 py-2 bg-green-400 hover:bg-green-700 rounded-md text-white">Edit</Link>
+                <Link confirm="Are you want to delete {{ $city->name }}?" confirm-button="Yes, delete!" cancel-button="No" method="DELETE" href="{{ route('admin.cities.destroy', $city) }}"
+                    class="px-3 py-2 bg-red-400 hover:bg-red-700 rounded-md text-white">Delete</Link>
+            </div>
+        @endcell
+    </x-splade-table>
+</x-admin-layout>
